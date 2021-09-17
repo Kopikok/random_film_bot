a285ec95bf7fe97eced8d0f30a1a23e9ffd4ddea
Kopikok <gleelu@yandex.ru>
2021-09-17 17:37:17 +0300

    Added good random funcs

--- bot.py
+++ bot.py
@@ -3,9 +3,8 @@ from secrets import omdb_api_token
 
 import telebot
 
-from tools import random_imdb_id
-from tools import get_movie_poster
 from tools import get_movie_info
+from tools import get_good_movie_info
 
 
 BOT = telebot.TeleBot(tg_api_token)
@@ -13,18 +12,39 @@ BOT = telebot.TeleBot(tg_api_token)
 
 @BOT.message_handler(commands=["start"])
 def start_chat(message):
-    BOT.send_message(message.chat.id, "Hello. I can send you a description of random movie, just type: /random")
+    BOT.send_message(message.chat.id, "Hello. I can send you a description of random movie, just type: /abs_random\n" +
+                     "or you can type: /good_random to get random movie with guaranteed description")
 
 
-@BOT.message_handler(commands=["random"])
-def randomize_film(message):
-    movie_id = random_imdb_id()
-    movie_poster = get_movie_poster(movie_id, omdb_api_token)
-    movie_info = get_movie_info(movie_id, omdb_api_token)
+@BOT.message_handler(commands=["help"])
+def send_help(message):
+    BOT.send_message(message.chat.id, "Type /abs_random to get info about random movie\n" +
+                     "Type /good_random to get info about random good movie" +
+                     "The good movie will have more information than other")
+
+
+@BOT.message_handler(commands=["abs_random"])
+def randomize_movie(message):
+    movie_info, movie_poster = get_movie_info(omdb_api_token)
     BOT.send_photo(chat_id=message.chat.id,
                    photo=movie_poster,
                    caption=movie_info)
-    movie_poster.close()
+    try:
+        movie_poster.close()
+    except AttributeError:
+        pass
+
+
+@BOT.message_handler(commands=["good_random"])
+def get_good_movie(message):
+    movie_info, movie_poster = get_good_movie_info(omdb_api_token)
+    BOT.send_photo(chat_id=message.chat.id,
+                   photo=movie_poster,
+                   caption=movie_info)
+    try:
+        movie_poster.close()
+    except AttributeError:
+        pass
 
 
 if __name__ == "__main__":
--- tools.py
+++ tools.py
@@ -4,23 +4,52 @@ import urllib
 import requests
 
 
-def get_movie_info(imdb_id: str, api_key: str) -> str:
+def get_movie_info(api_key):
+    movie_id = random_imdb_id()
+    movie_data = download_movie_data(movie_id, api_key)
+    while not movie_data:
+        movie_id = random_imdb_id()
+        movie_data = download_movie_data(movie_id, api_key)
+    return collect_description(movie_data), get_movie_poster(movie_id, api_key)
+
+
+def get_good_movie_info(api_key):
+    total_score = 0
+    while total_score < 4:
+        movie_id = random_imdb_id()
+        movie_data = download_movie_data(movie_id, api_key)
+        total_score = len(movie_data) - list(movie_data.values()).count('N/A')
+    return collect_description(movie_data), get_movie_poster(movie_id, api_key)
+
+
+def download_movie_data(imdb_id, api_key):
     omdb_url = "http://www.omdbapi.com/"
     my_params = {'i': imdb_id, 'apikey': api_key}
     movie_data = json.loads(requests.get(url=omdb_url, params=my_params).text)
+    try:
+        return {'Title': movie_data['Title'],
+                'Year': movie_data['Year'],
+                'Genre': movie_data['Genre'],
+                'imdbRating': movie_data['imdbRating'],
+                'imdbVotes': movie_data['imdbVotes'],
+                'Plot': movie_data['Plot']}
+    except KeyError:
+        return None
+
+
+def collect_description(movie_data):
     description = (f"{movie_data['Title']} ({movie_data['Year']})" +
                    f"\nGenres: {movie_data['Genre']}")
-
     if movie_data['imdbRating'] != 'N/A':
         description += f"\nIMDB rating: {movie_data['imdbRating']} / 10"
-    if movie_data['imdbVotes'] != 'N/A':
-        description += f"(based on {movie_data['imdbVotes']} votes)"
+        if movie_data['imdbVotes'] != 'N/A':
+            description += f"(based on {movie_data['imdbVotes']} votes)"
     if movie_data['Plot'] != 'N/A':
-        description += f"\n\n{movie_data['Plot']}"
+        description += f"\n{movie_data['Plot']}"
     return description
 
 
-def get_movie_poster(imdb_id: str, api_key: str):
+def get_movie_poster(imdb_id, api_key):
     url = f"http://img.omdbapi.com/?i={imdb_id}&apikey={api_key}"
     try:
         return urllib.request.urlopen(url).read()
